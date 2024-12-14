dd347d0330729b94751c225d35fb0e1351269350
hugo-andriamaromanana <hugo.andriamaromanana@laplateforme.io>
2024-12-14 18:27:00 +0100

    [wip] actions

--- gabolls/models/action.py
+++ gabolls/models/action.py
@@ -1,33 +1,71 @@
 from dataclasses import dataclass
 from enum import StrEnum, auto
-
+from gabolls.game.decisions import DrawDecisionType, InHandDecisionType
 from gabolls.models.card import Card
+from gabolls.models.player import Player
 
 
 class ActionType(StrEnum):
+    SWAP = auto()
     DRAW = auto()
     THROW = auto()
     DISCARD = auto()
     GABO = auto()
     EXCHANGE = auto()
+    PEAK = auto()
+
+
+class PeakOwner(StrEnum):
+    SELF = auto()
+    OTHER = auto()
+    DISCARD = auto()
+    DECK = auto()
 
 
 @dataclass
 class PlayerAction:
+    player: Player
     type: ActionType
 
 
 @dataclass
-class Draw(PlayerAction):
+class PeakCardAction(PlayerAction):
+    card: Card
+    owner: PeakOwner
+
+
+@dataclass
+class DrawFromDeckAction(PlayerAction):
+    decision_type: DrawDecisionType
+
+
+@dataclass
+class DrawFromDiscardAction(PlayerAction):
+    decision_type: DrawDecisionType
+
+
+@dataclass
+class InHandDiscardToPile(PlayerAction):
+    decision_type: InHandDecisionType
     card: Card
 
 
 @dataclass
-class Throw(PlayerAction):
-    card: Card
+class InHandSwapAction(PlayerAction):
+    decision_type: InHandDecisionType
+    owner_card: Card
+    in_hand_card: Card
 
 
 @dataclass
-class Exchange(PlayerAction):
-    entree_card: Card
-    exit_card: Card
+class BlindExchangeCardAction(PlayerAction):
+    player_card: Card
+    other_player: Player
+    other_player_card: Card
+
+
+@dataclass
+class ViewExchangeCardAction(PlayerAction):
+    player_card: Card
+    other_player: Player
+    other_player_card: Card
