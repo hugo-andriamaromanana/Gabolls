5d19ff367a8978254786bf1388e1c617707f080e
hugo-andriamaromanana <hugo.andriamaromanana@laplateforme.io>
2024-12-27 12:06:14 +0100

    [chore] score simpler

--- gabolls/models/score.py
+++ gabolls/models/score.py
@@ -1,13 +1,6 @@
-from gabolls.game.scoring import infer_score_from_round_end
 from gabolls.models.round_end import RoundEnd
-from gabolls.models.rules import Rules
 
 
 class Score:
     points: int
     round_ends: list[RoundEnd]
-    rules: Rules
-
-    def update(self, round_end: RoundEnd) -> None:
-        self.round_ends.append(round_end)
-        self.points = infer_score_from_round_end(self.points, round_end, self.rules)
