4f80faed7fcf25a59ad45456ed6fbfb0df25f675
hugo-andriamaromanana <hugo.andriamaromanana@laplateforme.io>
2024-12-27 12:08:51 +0100

    [feat] player swaps card

--- gabolls/models/hand.py
+++ gabolls/models/hand.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
-from gabolls.models.card import Card
+from gabolls.models.card import Card, CardView
 from gabolls.models.errors import CardNotInPlayerHand
 
 
 @dataclass
 class Hand:
     cards: list[Card]
+    card_views: list[CardView]
 
     def discard(self, card: Card) -> None:
         self.cards.remove(card)
@@ -24,12 +25,12 @@ class Hand:
     def indexes(self) -> list[int]:
         return list(range(self.size))
 
-    def swap(self, card: Card, other_card: Card) -> Card:
-        if card not in self.cards:
-            raise CardNotInPlayerHand(f"card: {card}, not in player Hand")
-        player_card_index = self.cards.index(card)
+    def swap(self, self_card: Card, other_card: Card) -> Card:
+        if self_card not in self.cards:
+            raise CardNotInPlayerHand(f"card: {self_card}, not in player Hand")
+        player_card_index = self.cards.index(self_card)
         self.cards[player_card_index] = other_card
-        return card
+        return self_card
 
     @property
     def is_empty(self) -> bool:
