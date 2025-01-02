9a1e09c240ac9595420c1c50c8c580d8770142b3
hugo-andriamaromanana <hugo.andriamaromanana@laplateforme.io>
2025-01-02 14:29:28 +0100

    [feat] player end rounds has players

--- gabolls/models/round_end.py
+++ gabolls/models/round_end.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
 from enum import StrEnum, auto
 
+from gabolls.models.player import Player
+
 
 class RoundEndType(StrEnum):
     SAFE = auto()
+    NEUTRAL_COUNTER = auto()
+    COUNTER = auto()
     SCORING = auto()
     SMALL_PENALTY = auto()
     LARGE_PENALTY = auto()
@@ -14,5 +18,6 @@ class RoundEndType(StrEnum):
 
 @dataclass
 class RoundEnd:
+    player: Player
     new_points: int
-    type: type
+    type: RoundEndType
