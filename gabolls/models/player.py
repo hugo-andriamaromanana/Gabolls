e55ac16cafd71f4a00ea376f6166ffb38f10493b
hugo-andriamaromanana <hugo.andriamaromanana@laplateforme.io>
2025-01-02 14:29:14 +0100

    [feat] uses player hash

--- gabolls/models/player.py
+++ gabolls/models/player.py
@@ -3,13 +3,12 @@ from gabolls.models.card import CardView
 from gabolls.models.hand import Hand
 from gabolls.models.profile import Profile
 from gabolls.models.rules import Rules
-from gabolls.models.score import Score
 
 
 @dataclass
 class Player:
     profile: Profile
-    score: Score
+    score: int
     hand: Hand
     rules: Rules
 
@@ -18,7 +17,7 @@ class Player:
 
     @property
     def lost(self) -> bool:
-        return self.score.points > self.rules.round_win_cap
+        return self.score > self.rules.round_win_cap
 
     @property
     def true_score(self) -> int:
@@ -27,3 +26,6 @@ class Player:
     @property
     def is_eligible(self) -> bool:
         return self.true_score <= self.rules.round_win_cap
+
+    def __hash__(self) -> int:
+        return hash(self.profile)
