51bb32922a2dacf1306464476aa591f8cb9f8e64
hugo-andriamaromanana <hugo.andriamaromanana@laplateforme.io>
2025-01-04 02:02:25 +0100

    [chore] splits discards logic

---
+++ gabolls/game/discard.py
@@ -0,0 +1,39 @@
+from gabolls.models.decisions import DiscardFromHandDecision
+from gabolls.models.discard import DiscardRequests, DiscardResultType
+from gabolls.models.errors import NoDecisionsTaken
+from gabolls.models.player_action import (
+    DiscardFromHandAction,
+    DrawPunishementCardAction,
+    PlayerAction,
+)
+from gabolls.models.round import Round
+
+
+def solve_discard(round: Round, discard_requests: DiscardRequests) -> Round:
+    responses = discard_requests.resolve(round)
+    for response in responses:
+        if response.result is DiscardResultType.FAIL:
+            punishment_card = round.deck.draw_top_card()
+            response.player.hand.add(punishment_card)
+
+            failed_discard_decision = DiscardFromHandDecision(response.card)
+            draw_punishement_action = DrawPunishementCardAction(punishment_card)
+            player_action = PlayerAction(
+                response.player, failed_discard_decision, draw_punishement_action
+            )
+
+        elif response.result is DiscardResultType.SUCESS:
+            response.player.hand.discard(response.card)
+            success_discard_decision = DiscardFromHandDecision(response.card)
+            player_discard_from_hand_action = DiscardFromHandAction(response.card)
+            player_action = PlayerAction(
+                response.player,
+                success_discard_decision,
+                player_discard_from_hand_action,
+            )
+
+        else:
+            raise NoDecisionsTaken
+
+        round.actions.append(player_action)
+    return round
